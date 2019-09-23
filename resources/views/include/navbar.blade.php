681d2b7bdfcf660cadcff8d4ac34dca29164de71
DaliaOmerabi <42964179+DaliaOmerabi@users.noreply.github.com>
2019-09-23 14:39:10 +0200

    bv

---
+++ resources/views/include/navbar.blade.php
@@ -0,0 +1,74 @@
+
+    <div class="site-navbar-wrap js-site-navbar bg-white">
+
+            <div class="container-fluid">
+              <div class="site-navbar bg-light">
+                <div class="py-1">
+                  <div class="row align-items-center">
+                    <div class="col-3">
+                  <div class="mb-0 site-logo"><a href="index.html"><img src=" {{asset('asset/images/logo.png')}} " width="95%"></a></div>
+                    </div>
+                    <div class="col-9">
+                      <nav class="site-navigation" role="navigation">
+                        <div class="container">
+                          <div class="d-inline-block d-lg-none ml-md-0 mr-auto py-3">
+                            <a href="#" class="site-menu-toggle js-menu-toggle text-black">
+                              <span class="icon-menu h3"></span>
+                             </a>
+                            </div>
+      
+                          <ul class="site-menu js-clone-nav d-none d-lg-block">
+                            <li><a href="index.html">الرئيسية</a></li>
+                            <li><a href="#" data-toggle="modal" data-target="#mycv">أضف سيرتك الذاتية</a></li> 
+                            <li class="has-children">
+                              <a href="">الوظائف</a>
+                              <ul class="dropdown arrow-top text-center" style="width:30rem;">
+                                  <table class="table table-borderless">
+                                   <tr>
+                                        <th scope="col">الدولة</th>
+                                        <th scope="col">الصنف</th>
+                                        <th scope="col">المدينة</th>
+                                        <th scope="col">اخرى</th> 
+                                       </tr> 
+                                      <tr>
+                                          <td>السودان(15)</td>
+                                          <td> تقانةالمعلومات</td> 
+                                          <td>الخرطوم</td> 
+                                          <td>المرتب</td>
+                                      </tr>
+                                      <tr>
+                                          <td>مصر(20)</td> 
+                                          <td>الطب</td>
+                                          <td>القاهرة</td>
+                                          <td>المرتب</td>
+                                        </tr>
+                                        <tr>
+                                            <td>الكويت(10)</td>
+                                            <td>هندسة</td> 
+                                            <td>عمان</td>
+                                            <td>المرتب</td>
+                                          </tr>
+                                          <tr>
+                                              <td>السعودية(13)</td> 
+                                              <td>الادارة</td> 
+                                              <td>الرياض</td>
+                                              <td>المرتب</td>
+                                            </tr> 
+                                  </table>  
+                              </ul>
+                            </li>
+                            <li><a href="candidate.html">صاحب عمل؟</a></li>
+                            <li><a href="about.html">إتصل بنا</a></li>
+                            <li><a href="candidate.html" class="add"><span> + </span> أضف وظيفة جديدة</a></li>
+                            <li style="margin: 0 0 0 -65px;"><a href="#" style="margin: 0 6px 0 -18px" data-toggle="modal" data-target="#login"> <span class=""><img src=" {{asset('asset/images/login.png')}} " class="pl-1" alt=""></span>دخول/</a><a href="#" data-toggle="modal" data-target="#register">تسجيل جديد</a></li> 
+                           <li ><a href="about.html" class="text-right" >Eng<img SRC=" {{asset('asset/images/en.png')}} " width="20%" class="rounded-circle border border-light"></a></li>
+                          </ul> 
+                        </div>
+                      </nav>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+      
