15a902f320a4a90915317fe18b0eac144aa46e45
DaliaOmerabi <42964179+DaliaOmerabi@users.noreply.github.com>
2019-09-24 14:40:41 +0200

    design

---
+++ resources/views/pages/cvrequest.blade.php
@@ -0,0 +1,166 @@
+@extends('layouts.defult')
+@section('content')
+
+<div class="unit-5 overlay" style="background-image: url('images/hero_1.jpg');">
+    <div class="container text-center">
+        <h2 class="h4mb-0">جرّب البحث مجاناً. لن تحتاج لاستخدام بطاقة ائتمان</h2>
+        <p class="mb-0 unit-6 p-3"><a href="index.html">الرئيسية</a> <span class="sep">></span> <span>البحث عن
+                السيرة الذاتية</span></p>
+    </div>
+</div>
+
+<!-- Start post Area -->
+<section class="post-area section-gap">
+        <div class="container bg-light">
+            <div class="row justify-content-center d-flex">
+                <div class="col-lg-8 post-list"> 
+                    <div class="single-post justify-content-between">
+                        <div class="thumb d-flex flex-row justify-content-between">
+                        <div class="title d-flex flex-row">
+                            <img src=" {{asset('asset/images/person_2.jpg')}} " width="110px" class=" pl-1" alt=""> 
+                              <div class="titles"> 
+                                 <p class="m-0">لينة <span>-</span><span> مطور برمجيات </span></p>
+                                  <h6>الجنسية: السودان</h6>
+                                    <div class=" d-flex flex-row">			
+                                    <h6>العمر: 22سنة</h6>&ensp;&ensp;
+                                    <h6>المؤهل: بكالريوس</h6>
+                                </div>	
+                                </div> 
+                             </div> 
+                             <ul class="btns"> 
+                                <li><a href="#" class="add">إشترك الان</a></li>
+                            </ul>
+                         </div>
+                        <div class="details"> 
+                            <hr>
+                            <p>. .مطور ويب. مطوربرمجيات .مطور برمجيات .مطور ويب تطوير بعض العادات لدى المستخدمين مطوربرمجيات الآداء الوظيفي لدى الموظفين. مطوربرمجيات Web and mobile ... </p>
+                            <p class=" ">طبيعة العمل: Full time</p>
+                            <p class=" "><span class="lnr lnr-map"></span>سنوات الخبرة: سنة و6 أشهر</p>
+                            <p class=" "><span class="lnr lnr-database"></span> الراتب المتوقع: 15k - 25k</p>
+                        </div>
+                    </div>
+                    
+                    <div class="single-post justify-content-between">
+                            <div class="thumb d-flex flex-row justify-content-between">
+                            <div class="title d-flex flex-row">
+                                <img src=" {{asset('asset/images/person_2.jpg')}} " width="110px" class=" pl-1" alt=""> 
+                                  <div class="titles"> 
+                                     <p class="m-0">لينة <span>-</span><span> مطور برمجيات </span></p>
+                                      <h6>الجنسية: السودان</h6>
+                                        <div class=" d-flex flex-row">			
+                                        <h6>العمر: 22سنة</h6>&ensp;&ensp;
+                                        <h6>المؤهل: بكالريوس</h6>
+                                    </div>	
+                                    </div> 
+                                 </div> 
+                                 <ul class="btns"> 
+                                    <li><a href="#" class="add">إشترك الان</a></li>
+                                </ul>
+                             </div>
+                            <div class="details"> 
+                                <hr>
+                                <p>. .مطور ويب. مطوربرمجيات .مطور برمجيات .مطور ويب تطوير بعض العادات لدى المستخدمين مطوربرمجيات الآداء الوظيفي لدى الموظفين. مطوربرمجيات Web and mobile ... </p>
+                                <p class=" ">طبيعة العمل: Full time</p>
+                                <p class=" "><span class="lnr lnr-map"></span>سنوات الخبرة: سنة و6 أشهر</p>
+                                <p class=" "><span class="lnr lnr-database"></span> الراتب المتوقع: 15k - 25k</p>
+                            </div>
+                        </div>
+
+                        <div class="single-post justify-content-between">
+                                <div class="thumb d-flex flex-row justify-content-between">
+                                <div class="title d-flex flex-row">
+                                    <img src=" {{asset('asset/images/person_2.jpg')}} " width="110px" class=" pl-1" alt=""> 
+                                      <div class="titles"> 
+                                         <p class="m-0">احمد <span>-</span><span> مطور برمجيات </span></p>
+                                          <h6>الجنسية: السودان</h6>
+                                            <div class=" d-flex flex-row">			
+                                            <h6>العمر: 22سنة</h6>&ensp;&ensp;
+                                            <h6>المؤهل: بكالريوس</h6>
+                                        </div>	
+                                        </div> 
+                                     </div> 
+                                     <ul class="btns"> 
+                                        <li><a href="#" class="add">إشترك الان</a></li>
+                                    </ul>
+                                 </div>
+                                <div class="details"> 
+                                    <hr>
+                                    <p>. .مطور ويب. مطوربرمجيات .مطور برمجيات .مطور ويب تطوير بعض العادات لدى المستخدمين مطوربرمجيات الآداء الوظيفي لدى الموظفين. مطوربرمجيات Web and mobile ... </p>
+                                    <p class=" ">طبيعة العمل: Full time</p>
+                                    <p class=" "><span class="lnr lnr-map"></span>سنوات الخبرة: سنة و6 أشهر</p>
+                                    <p class=" "><span class="lnr lnr-database"></span> الراتب المتوقع: 15k - 25k</p>
+                                </div>
+                            </div>
+
+                            <div class="single-post justify-content-between">
+                                    <div class="thumb d-flex flex-row justify-content-between">
+                                    <div class="title d-flex flex-row">
+                                        <img src=" {{asset('asset/images/person_3.jpg')}} " width="110px" class=" pl-1" alt=""> 
+                                          <div class="titles"> 
+                                             <p class="m-0">محمد <span>-</span><span> مطور برمجيات </span></p>
+                                              <h6>الجنسية: السودان</h6>
+                                                <div class=" d-flex flex-row">			
+                                                <h6>العمر: 22سنة</h6>&ensp;&ensp;
+                                                <h6>المؤهل: بكالريوس</h6>
+                                            </div>	
+                                            </div> 
+                                         </div> 
+                                         <ul class="btns"> 
+                                            <li><a href="#" class="add">إشترك الان</a></li>
+                                        </ul>
+                                     </div>
+                                    <div class="details"> 
+                                        <hr>
+                                        <p>. .مطور ويب. مطوربرمجيات .مطور برمجيات .مطور ويب تطوير بعض العادات لدى المستخدمين مطوربرمجيات الآداء الوظيفي لدى الموظفين. مطوربرمجيات Web and mobile ... </p>
+                                        <p class=" ">طبيعة العمل: Full time</p>
+                                        <p class=" "><span class="lnr lnr-map"></span>سنوات الخبرة: سنة و6 أشهر</p>
+                                        <p class=" "><span class="lnr lnr-database"></span> الراتب المتوقع: 15k - 25k</p>
+                                    </div>
+                                </div>
+                     		
+                   
+                         
+                    <div class="col-md-12 text-center mt-5 add-more text-white" data-aos="fade-up" data-aos-delay="50">
+                            <a href="#" class="btn rounded p-2 mb-5"><span class="icon-plus-circle"></span>عرض المزيد  </a>
+                          </div>
+                 
+                </div>
+                <div class="col-lg-4 sidebar">
+                    <div class="single-slidebar">
+                        <h4>Jobs by Location</h4>
+                        <ul class="cat-list">
+                            <li><a class="justify-content-between d-flex" href="category.html"><p>New York</p><span>37</span></a></li>
+                            <li><a class="justify-content-between d-flex" href="category.html"><p>Park Montana</p><span>57</span></a></li>
+                            <li><a class="justify-content-between d-flex" href="category.html"><p>Atlanta</p><span>33</span></a></li>
+                            <li><a class="justify-content-between d-flex" href="category.html"><p>Arizona</p><span>36</span></a></li>
+                            <li><a class="justify-content-between d-flex" href="category.html"><p>Florida</p><span>47</span></a></li>
+                            <li><a class="justify-content-between d-flex" href="category.html"><p>Rocky Beach</p><span>27</span></a></li>
+                            <li><a class="justify-content-between d-flex" href="category.html"><p>Chicago</p><span>17</span></a></li>
+                        </ul>
+                    </div>
+
+                                         
+
+                    <div class="single-slidebar">
+                        <h4>Top rated job posts</h4>
+                        <ul class="cat-list">
+                            <li><a class="justify-content-between d-flex" href="category.html"><p>Technology</p><span>37</span></a></li>
+                            <li><a class="justify-content-between d-flex" href="category.html"><p>Media & News</p><span>57</span></a></li>
+                            <li><a class="justify-content-between d-flex" href="category.html"><p>Goverment</p><span>33</span></a></li>
+                            <li><a class="justify-content-between d-flex" href="category.html"><p>Medical</p><span>36</span></a></li>
+                            <li><a class="justify-content-between d-flex" href="category.html"><p>Restaurants</p><span>47</span></a></li>
+                            <li><a class="justify-content-between d-flex" href="category.html"><p>Developer</p><span>27</span></a></li>
+                            <li><a class="justify-content-between d-flex" href="category.html"><p>Accounting</p><span>17</span></a></li>
+                        </ul>
+                    </div>
+
+             
+                </div>
+            </div>
+        </div>	
+    </section>
+    <!-- End post Area -->
+
+
+
+@endsection
---
+++ resources/views/pages/jobowner.blade.php
@@ -0,0 +1,133 @@
+@extends('layouts.defult')
+@section('content')
+    
+
+<div class="unit-5 overlay  " style="background-image: url(' {{asset('asset/images/hero_1.jpg')}} ');">
+    <div class="container">
+       <div class="row align-items-center">
+           <div class="col-12" data-aos="fade">
+               <div class="pb-4">
+                   <h1 class="h3 text-center text-white pt-2">
+                       وظّف الباحثين عن عمل بسرعة  
+                       ابحث من بين 36,800,000 سيرة ذاتية</h1>
+                   </div>
+               <form action="#">
+                   <div class="row mb-3">
+                       <div class="col-md-9">
+                           <div class="row">
+                               <div class="col-md-6 mb-3 mb-md-0">
+                                   <div class="input-wrap">
+                                       <span class="icon icon-keyboard"></span>
+                                       <input type="text" class=" form-control border-0 px-3"
+                                           placeholder="المسمى الوظيفي أو المهارة  ">
+                                   </div>
+                               </div>
+                               <div class="col-md-6 mb-3 mb-md-0">
+                                   <div class="input-wrap">
+                                       <span class="icon icon-room"></span>
+                                       <input type="text" class="form-control border-0 px-3" id="autocomplete"
+                                           placeholder="المدينة أو المقاطعة أو المنطقة" onFocus="geolocate()">
+                                   </div>
+                               </div>
+                           </div>
+                       </div>
+                       <div class="col-md-3">
+                           <input type="submit" class="btn btn-search  btn-block" style="    background-color: transparent!important;
+                           border: 2px solid #fff;
+                           border-radius: 5px;
+                           color: #fff;
+                           font-size: 20px;"
+                               value="ابحث السيرة الذاتية" data-toggle="modal" data-target="#exampleModal">
+                       </div>
+                   </div>
+               </form>
+           </div>
+       </div>
+   </div>
+</div>
+
+
+<!--start feature-->
+<div class="features text-center">
+       <div class="container">
+           <div class="row">
+               <div class="col-sm-6 col-md-4">  <img src="{{asset('asset/images/ideaicon2.png')}}" alt="">
+                   <h3>Great idea</h3>
+                   <p>Lorem ipsum dolor sit amet consect adipisicing elit Aperiam dolore repellendus veniam vero numquam	dolor autem reiciendis sapiente.</p>
+               </div>
+               <div class="col-sm-6 col-md-4"> <img src="{{asset('asset/images/ideaicon.png')}}">
+                   <h3>Great idea</h3>
+                   <p>Lorem ipsum dolor sit amet consect adipisicing elit Aperiam dolore repellendus veniam vero numquam dolor autem reiciendis sapiente.</p>
+               </div>
+               <div class="col-sm-6 col-md-4"> <img src="{{asset('asset/images/light-bulb.png')}}" width="6%" alt="">
+                   <h3>Great idea</h3>
+                   <p>Lorem ipsum dolor sit amet consect adipisicing elit Aperiam dolore repellendus veniam vero numquam dolor autem reiciendis sapiente.</p>
+               </div> 
+           </div>
+        </div>
+   </div>
+<!--ends feature-->
+
+<!--start pricing-->
+<div class="pricing text-center">
+<div class="container">
+   <h1>Pricing Table</h1>
+       <div class="section"><p>Lorem ipsum dolor sit amet consect elit Aperiam dolore repellendus dolor autem Lorem ipsum dolor sit amet consect elit Aperiam dolore repellendus veniam lorem ipsum dolor sit amet consect elit Aperiam dolore repellendus dolor autem Lorem</p>
+       </div>	<div class="row">
+               <div class=" col-md-4 col-sm-6">
+                <div class="card">
+                    <div class="card-body">
+                           <h4 class="card-title">Small Business</h4>
+                            <h6 class="card-subtitle mb-2 text-muted">Lorem ipsum</h6>
+                            <p class="card-text">299$/<span>Year</span> </p>
+                            <ul class="list-group list-group-flush">
+                               <li class="list-group-item">Cras justo odio</li>
+                               <li class="list-group-item">Dapibus ac facilisis in</li>
+                               <li class="list-group-item">Vestibulum at eros</li>
+                               <li class="list-group-item">Cras justo odio</li>
+                               <li class="list-group-item">Dapibus ac facilisis in</li>
+                           </ul>
+                               <a href="#" class="card-link">Read more</a>
+                       </div>
+                   </div>
+               </div>
+               <div class="col-md-4 col-sm-6">
+                <div class="card corp">
+                    <div class="card-body ">
+                           <h4 class="card-title ">Corporate</h4>
+                            <h6 class="card-subtitle mb-2 text-muted">Lorem ipsum</h6>
+                                <p class="card-text">199$/ <span>Year</span> </p>
+                                <ul class="list-group list-group-flush">
+                               <li class="list-group-item">Cras justo odio</li>
+                               <li class="list-group-item">Dapibus ac facilisis in</li>
+                               <li class="list-group-item">Vestibulum at eros</li>
+                                   <li class="list-group-item">Cras justo odio</li>
+                                   <li class="list-group-item">Dapibus ac facilisis in</li>
+                             </ul>
+                                <a href="#" class="card-link">Read more</a>
+                       </div>
+                   </div>
+               </div>
+               <div class="col-md-4 col-sm-12">
+                <div class="card">
+                         <div class="card-body">
+                           <h4 class="card-title">Enterprise</h4>
+                            <h6 class="card-subtitle mb-2 text-muted">Lorem ipsum</h6>
+                            <p class="card-text">99$/ <span>Year</span> </p>
+                            <ul class="list-group list-group-flush">
+                               <li class="list-group-item">Cras justo odio</li>
+                               <li class="list-group-item">Dapibus ac facilisis in</li>
+                               <li class="list-group-item">Vestibulum at eros</li>
+                               <li class="list-group-item">Cras justo odio</li>
+                               <li class="list-group-item">Dapibus ac facilisis in</li>
+                           </ul>
+                               <a href="#" class="card-link">Read more</a>
+                       </div>
+                   </div>
+               </div>
+           </div>
+       </div>
+</div>
+
+
+@endsection
