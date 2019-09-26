ec1a05678ecbb626d37fe00285c9bfb2db3589f4
Mohamed Amin <39481290+Xm3a01@users.noreply.github.com>
2019-09-26 13:36:47 +0200

    change

--- app/Http/Controllers/BrowseController.php
+++ app/Http/Controllers/BrowseController.php
@@ -11,27 +11,12 @@ class BrowseController extends Controller
         return view('pages.home');
     }
 
-    public function ar_index()
-    {
-        return view('pages.ar_home');
-    }
-
     public function about()
     {
         
     }
 
-    public function ar_about()
-    {
-        
-    }
-
     public function search()
-    {
-        
-    }
-
-    public function ar_search()
     {
         
     }
--- routes/web.php
+++ routes/web.php
@@ -5,34 +5,17 @@
 Route::get('/','BrowseController@index');
 
 
-Route::get('tests','Test@index');
-Route::get('two', 'Test@testtow');
-Route::post('tests/one', 'Test@store')->name('one.store');
-Route::post('tests/two', 'Test@store2')->name('two.store');
 
 
 Auth::routes();
-
-Route::get('/home', 'HomeController@index')->name('home');
-
-Auth::routes();
-
-Route::get('/home', 'HomeController@index')->name('home');
-
-
-Route::get('/home', function () {
-});
-Route::get('/home-d', function () {
-
-    return view('layouts.defult');
-});
-// route::get('/', function(){
-//     return view('pages.home');
-// });
 
 
 //test Route
 
+Route::get('tests','Test@index');
+Route::get('two', 'Test@testtow');
+Route::post('tests/one', 'Test@store')->name('one.store');
+Route::post('tests/two', 'Test@store2')->name('two.store');
 route::get('/datas/{id}', 'Test@select');
 
 route::get('/search/test', function(){
